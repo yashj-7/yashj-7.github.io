e7daed7086204eeffe1f28dd4d6d0d93f271a9e0
unknown <yash1911060@akgec.ac.in>
2020-04-22 00:37:51 +0530

    initial commit

---
+++ ui.js
@@ -0,0 +1,25 @@
+ document.getElementById("card1").onmouseover = function(){
+            document.getElementById("bdy1").style.display="block";
+        };
+          document.getElementById("card1").onmouseout = function(){
+               document.getElementById("bdy1").style.display="none";
+          }
+          document.getElementById("card2").onmouseover = function(){
+            document.getElementById("bdy2").style.display="block";
+        };
+          document.getElementById("card2").onmouseout = function(){
+               document.getElementById("bdy2").style.display="none";
+          }
+           document.getElementById("card3").onmouseover = function(){
+            document.getElementById("bdy3").style.display="block";
+        };
+          document.getElementById("card3").onmouseout = function(){
+               document.getElementById("bdy3").style.display="none";
+          }
+           document.getElementById("card4").onmouseover = function(){
+            document.getElementById("bdy4").style.display="block";
+        };
+          document.getElementById("card4").onmouseout = function(){
+               document.getElementById("bdy4").style.display="none";
+          }
+           
