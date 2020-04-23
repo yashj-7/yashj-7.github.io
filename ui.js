1e28e5a1b22c1832a45ceb494ed28565f3a32a0f
unknown <yash1911060@akgec.ac.in>
2020-04-23 22:27:44 +0530

    completed3

---
+++ ui.js
@@ -0,0 +1,52 @@
+ document.getElementById("card1").onmouseover = function(){
+            document.getElementById("bdy1").style.display="block";
+        };
+          document.getElementById("card1").onmouseout = function(){
+               document.getElementById("bdy1").style.display="none";
+          }
+          document.getElementById("card2").onmouseover = function(){
+            document.getElementById("bdy2").style.display="block";
+        };
+          document.getElementById("card2").onmouseout = function(){
+               document.getElementById("bdy2").style.display="none";
+          }
+           document.getElementById("card3").onmouseover = function(){
+            document.getElementById("bdy3").style.display="block";
+        };
+          document.getElementById("card3").onmouseout = function(){
+               document.getElementById("bdy3").style.display="none";
+          }
+           document.getElementById("card4").onmouseover = function(){
+            document.getElementById("bdy4").style.display="block";
+        };
+          document.getElementById("card4").onmouseout = function(){
+               document.getElementById("bdy4").style.display="none";
+          }
+         
+            document.getElementById("c1").onmouseover = function(){
+            document.getElementById("b1").style.display="block";
+        };
+          document.getElementById("c1").onmouseout = function(){
+               document.getElementById("b1").style.display="none";
+          }
+
+            document.getElementById("c2").onmouseover = function(){
+            document.getElementById("b2").style.display="block";
+        };
+          document.getElementById("c2").onmouseout = function(){
+               document.getElementById("b2").style.display="none";
+          }
+
+            document.getElementById("c3").onmouseover = function(){
+            document.getElementById("b3").style.display="block";
+        };
+          document.getElementById("c3").onmouseout = function(){
+               document.getElementById("b3").style.display="none";
+          }
+
+            document.getElementById("c4").onmouseover = function(){
+            document.getElementById("b4").style.display="block";
+        };
+          document.getElementById("c4").onmouseout = function(){
+               document.getElementById("b4").style.display="none";
+          }
